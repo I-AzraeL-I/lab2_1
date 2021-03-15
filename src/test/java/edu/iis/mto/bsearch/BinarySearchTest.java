83aa074b7272c9154fb064b29034e210615d6c5a
Kacper.Gajewski <222431@edu.p.lodz.pl>
2021-03-15 17:43:41 +0100

    testy_v1 Implemented test no.1, fix result

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -1,6 +1,7 @@
 package edu.iis.mto.bsearch;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,8 +13,13 @@ class BinarySearchTest {
     void setUp() throws Exception {}
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void SequenceLengthEqualsOneAndElementInSequence() {
+        int[] seq = {5};
+        int key = 5;
+
+        SearchResult result = BinarySearch.search(key, seq);
+        assertTrue(result.isFound());
+        assertEquals(key, seq[result.getPosition()]);
     }
 
 }
